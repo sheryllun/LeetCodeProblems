6b60aa83a2935e020caa73710e47a18577fbd7e7
sheryllun <sheryl.v.lun@gmail.com>
2016-10-17 00:07:32 -0400

    Initial add files

---
+++ TwoSum.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] solution = new int[2];
+        for(int i = 0; i < nums.length; i++) {
+          for(int j = i + 1; j < nums.length; j++) {
+            // if(nums[j] > target - nums[i]) {
+            //   break;
+            // }
+            if(nums[i] + nums[j] == target) {
+              solution[0] = i;
+              solution[1] = j;
+              return solution;
+            }
+          }
+        }
+        return solution;
+    }
+}
